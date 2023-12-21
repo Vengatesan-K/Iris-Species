aac0bee5744dc36a247e5a83237ec79b2d6786fc
Vengatesan K <128688827+Vengatesan-K@users.noreply.github.com>
2023-12-21 18:49:49 +0530

    Update App.py

--- App.py
+++ App.py
@@ -1,35 +1,37 @@
 from flask import Flask, render_template, request
 import pickle
+import numpy as np
+from sklearn.preprocessing import StandardScalar
+import joblib as joblib
+import os
+
+model = joblib.load('best_log_model.pkl')
 
 app = Flask(__name__)
-#loading the model
-model = pickle.load(open('iris_best_model.sav', 'rb'))
+
+IMG_FOLDER = os.path.join('Styling','IMG')
+app.config['UPLOAD_FOLDER'] = IMG_FOLDER
+
 
 @app.route('/')
-def home():
-    result = ""
-    return render_template('index.html', result=result)
+def index():
+    return render_template('index.html')
 
-@app.route('/predict', methods=['POST', 'GET'])
-def predict():
+@app.route('/', methods=['POST', 'GET'])
+def home():
     if request.method == 'POST':
         sepal_length = float(request.form['sepal_length'])
         sepal_width =  float(request.form['sepal_width'])
         petal_length = float(request.form['petal_length'])
         petal_width = float(request.form['petal_width'])
-        result = model.predict([[sepal_length, sepal_width, petal_length, petal_width]])
-        if result == 0:
-            result = 'Iris Setosa'
-        elif result == 1:
-            result = 'Iris Versicolour'
-        elif result == 2:
-            result = 'Iris Virginica'
-        else:
-            result = 'Error'
-        return render_template('index.html', result=result)
+        data = np.array([[sepal_length,sepal_width,petal_length,petal_width]],dtype = float)
+        result = model.predict(data)
+        image = result[0] + '.png'
+        image = os.path.join(app.config['UPLOAD_FOLDER'], image)
+        return render_template('index.html', result=result[0],image=image)
     
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True,debug=True,host='0.0.0.0',port=8080)
 
