bef1482898c8c92f4a20e140d6b256c01ab59c61
Vengatesan K <128688827+Vengatesan-K@users.noreply.github.com>
2023-12-21 19:39:09 +0530

    Update App.py

--- App.py
+++ App.py
@@ -25,7 +25,7 @@ def home():
         pw = request.form['PetalWidth']
         data = np.array([[sl,sw,pl,pw]], dtype = float)
         prediction = model.predict(data)
-        image = prediction[0] + '.png'
+        image = str(prediction[0]) + '.png'
         image = os.path.join(app.config['UPLOAD_FOLDER'], image)
         return render_template('index.html', prediction=prediction[0],image=image)
     
