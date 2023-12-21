a940b30d880fe250e1a26f249aa0a794dd6277a3
Vengatesan K <128688827+Vengatesan-K@users.noreply.github.com>
2023-12-21 20:40:24 +0530

    Update App.py

--- App.py
+++ App.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template, request
-import pickle
+from flask import Flask, request, render_template
 import numpy as np
+from sklearn.preprocessing import StandardScaler
 import joblib as joblib
 import os
 
@@ -16,21 +16,21 @@ app.config['UPLOAD_FOLDER'] = IMG_FOLDER
 def index():
     return render_template('index.html')
 
+
 @app.route('/', methods=['GET', 'POST'])
 def home():
     if request.method == 'POST':
         sl = request.form['SepalLength']
-        sw =  request.form['SepalWidth']
+        sw = request.form['SepalWidth']
         pl = request.form['PetalLength']
         pw = request.form['PetalWidth']
-        data = np.array([[sl,sw,pl,pw]], dtype = float)
+        data = np.array([[sl, sw, pl, pw]], dtype=float)
         prediction = model.predict(data)
         image = prediction[0] + '.png'
         image = os.path.join(app.config['UPLOAD_FOLDER'], image)
-        return render_template('index.html', prediction=prediction[0],image=image)
-    
+        return render_template('index.html', prediction=prediction[0], image=image)
     return render_template('index.html')
 
-if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0',port=8080)
 
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=8080)
